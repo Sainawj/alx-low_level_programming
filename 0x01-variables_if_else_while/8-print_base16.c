9eac61a5e0136cdbf00f0214bc09e8ca8c7a14d3
Jonathan  Saina <sainajonathan@gmail.com>
2023-02-20 06:23:33 -0800

    a program that prints all the numbers of base 16 in lowercase, followed by a new line

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,18 +9,18 @@ int main(void)
 {
 	char c = '0';
 
-		while (c <= '9')
-				{
-					putchar(c);
-					c++;
-				}
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
 	c = 'a';
 
-		while (c <= 'f')
-				{
-					putchar(c);
-					c++;
-				}
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
 
 	putchar('\n');
 	return (0);
