bbe4f59712df09a2b0cc531843641e8e0930067a
Jonathan Saina <sainajonathan@gmail.com>
2024-02-14 01:34:27 -0800

    My firt commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_txtfile- Read text to STDOUT
+ * @fname: text file to be read
+ * @letters: no of letters to be read
+ * Return: w- actual number of bytes 
+ * 0 when function fails or fname is NULL.
+ */
+ssize_t read_txtfile(const char *fname, size_t letters)
+{
+        char *chr;
+        ssize_t rf;
+        ssize_t wrt;
+        ssize_t read;
+
+        rf = open(fname, O_RDONLY);
+        if (rf == -1)
+                return (0);
+        chr = malloc(sizeof(char) * letters);
+        read = read(rf, buf, letters);
+        wrt = write(STDOUT_FILENO, chr, read);
+
+        free(chr);
+        close(rf);
+        return (wrt);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+ssize_t read_txtfile(const char *fname, size_t letters);
+int create_file(const char *fname, char *txt_content);
+int append_txt_to_file(const char *fname, char *txt_content);
+
+#endif
