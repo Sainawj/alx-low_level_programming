9fe9e2e3655d0463689774ee40e338bad01fd4ec
Jonathan Saina <sainajonathan@gmail.com>
2024-02-16 12:24:48 -0800

    20th commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,13 +4,13 @@
 
 /**
  * create_fl - creates a file
- * @txt_content: content file
+ * @text_content: content file
  * @fname: the variable pointer
  * Description: Creates function for making a file.
  * Return: -1 on failure, 1 on successful
  */
 
-int create_fl(const char *fname, char *txt_content)
+int create_fl(const char *fname, char *text_content)
 {
 	int i = 0, fl;
 
@@ -18,10 +18,10 @@ int create_fl(const char *fname, char *txt_content)
 		return (-1);
 
 	if (txt_content == NULL)
-		txt_content = "";
+		text_content = "";
 
 
-	while (txt_content[i] != '\0')
+	while (text_content[i] != '\0')
 	{
 		i++;
 	}
@@ -31,7 +31,7 @@ int create_fl(const char *fname, char *txt_content)
 	if (fl == -1)
 		return (-1);
 
-	write(fl, txt_content, i);
+	write(fl, text_content, i);
 
 	return (1);
 }
