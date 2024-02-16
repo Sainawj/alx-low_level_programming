aefce1d766ec7935035bcaaedfdfd2613c99b65d
Jonathan Saina <sainajonathan@gmail.com>
2024-02-16 10:54:49 -0800

    sith commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,9 +1,8 @@
-#include <stdio.h>
 #include "main.h"
 #include <stdlib.h>
 
 /**
- * read_txtfile- Read text to STDOUT
+ * read_textfile- Read text to STDOUT
  * @fname: text file to be read
  * @letters: no of letters to be read
  * Return: w- actual number of bytes
@@ -12,29 +11,29 @@
 
 ssize_t read_textfile(const char *fname, size_t letters)
 {
-	ssize_t file, let, wrt;
+	ssize_t f, let, wrt;
 	char *txt;
 
-	txt = malloc(letters);
+	text = malloc(letters);
 	if (txt == NULL)
 		return (0);
 
 	if (fname == NULL)
 		return (0);
 
-	file = open(fname, O_RDONLY);
+	f = open(fname, O_RDONLY);
 
-	if (file == -1)
+	if (f == -1)
 	{
 		free(txt);
 		return (0);
 	}
 
-	let = read(file, txt, letters);
+	let = read(f, txt, letters);
 
 	wrt = write(STDOUT_FILENO, txt, let);
 
-	close(file);
+	close(f);
 
 	return (wrt);
 }
