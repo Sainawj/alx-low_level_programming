8081ec31290387fba6dcaf58d6e8cb094e7d572c
Jonathan Saina <sainajonathan@gmail.com>
2024-03-21 06:54:37 -0700

    1st commit2

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * print_dlistint - prints all the elements of a
+ * dlistint_t list
+ * @h: head of the list
+ * Return: the number of nodes
+ */
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t node_count = 0;
