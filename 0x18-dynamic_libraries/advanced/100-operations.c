cff047f462c9b15dbe0af0a83b064b18c3816fd5
Jonathan Saina <sainajonathan@gmail.com>
2024-03-25 17:26:05 -0700

    Third commit2

--- 0x18-dynamic_libraries/advanced/100-operations.c
+++ 0x18-dynamic_libraries/advanced/100-operations.c
@@ -1,25 +1,64 @@
-int add(int a, int b) {
-	return a + b;
+/**
+ * add - Adds two integers
+ * @a: The first integer
+ * @b: The second integer
+ * Return: The sum of the two integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
 }
 
-int sub(int a, int b) {
-	return a - b;
+/**
+ * sub - Subtracts two integers
+ * @a: The first integer
+ * @b: The second integer
+ * Return: The result of the subtraction (a - b)
+ */
+int sub(int a, int b)
+{
+	return (a - b);
 }
 
-int mul(int a, int b) {
-	return a * b;
+/**
+ * mul - Multiplies two integers
+ * @a: The first integer
+ * @b: The second integer
+ * Return: The product of the two integers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
 }
 
-int div(int a, int b) {
-	if (b == 0) {
-	return -1;  // Return -1 for division by zero
-    }
-	return a / b;
+/**
+ * div - Divides two integers
+ * @a: The dividend
+ * @b: The divisor
+ * Return: The quotient of the division (a / b)
+ *         If division by zero, returns -1
+ */
+int div(int a, int b)
+{
+	if (b == 0)
+	{
+	return (-1);
+	}
+	return (a / b);
 }
 
-int mod(int a, int b) {
-	if (b == 0) {
-	return -1;  // Return -1 for division by zero
-    }
-	return a % b;
+/**
+ * mod - Computes the remainder of the division of two integers
+ * @a: The dividend
+ * @b: The divisor
+ * Return: The remainder of the division (a % b)
+ *         If division by zero, returns -1
+ */
+int mod(int a, int b)
+{
+	if (b == 0)
+	{
+	return (-1);
+	}
+	return (a % b);
 }
