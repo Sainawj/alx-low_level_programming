e4b1e157c72cd894a3cf49fd10e01a8658ee76fd
Jonathan Saina <sainajonathan@gmail.com>
2024-04-16 04:01:17 -0700

    Add untracked files

---
+++ 0x17-doubly_linked_lists/main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char *_strstr(char *haystack, char *needle);
+int _strcmp(char *s1, char *s2);
+int _isdigit(int c);
+char *_strcpy(char *dest, char *src);
+char *_strncpy(char *dest, char *src, int n);
+int _abs(int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+int _isalpha(int c);
+char *_strpbrk(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept);
+int _islower(int c);
+char *_memset(char *s, char b, unsigned int n);
+int _strlen(char *s);
+void _puts(char *s);
+char *_strcat(char *dest, char *src);
+int _isupper(int c);
+char *_strchr(char *s, char c);
+int _atoi(char *s);
+int _putchar(char c);
+char *_strncat(char *dest, char *src, int n);
+
+#endif
