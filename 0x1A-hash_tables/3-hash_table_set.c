3d0d3d80c1de682942e9d7db8f03f355d99ea155
Jonathan Saina <sainajonathan@gmail.com>
2024-04-16 03:03:51 -0700

    4th commit.1

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,48 +15,41 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = 0;
 	char *valuecopy, *keycopy;
-	hash_node_t *bucket, *new_node;
+	hash_node_t  *bucket, *new_node;
 
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-	return (0);
+	if (!ht || !key || !*key || !value)
+		return (0);
 
 	valuecopy = strdup(value);
-	if (valuecopy == NULL)
-	return (0);
+	if (!valuecopy)
+		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
 	bucket = ht->array[index];
 
 	while (bucket)
 	{
-	if (strcmp(key, bucket->key) == 0)
-	{
-		free(bucket->value);
-		bucket->value = valuecopy;
-		return (1);
+		if (!strcmp(key, bucket->key))
+		{
+			free(bucket->value);
+			bucket->value = valuecopy;
+			return (1);
+		}
+		bucket = bucket->next;
 	}
-	bucket = bucket->next;
-	}
-
-	new_node = (hash_node_t *)calloc(1, sizeof(hash_node_t));
+	new_node = calloc(1, sizeof(hash_node_t));
 	if (new_node == NULL)
 	{
-	free(valuecopy);
-	return (0);
-	}
-
-	keycopy = strdup(key);
-	if (keycopy == NULL)
-	{
 		free(valuecopy);
-		free(new_node);
 		return (0);
 	}
-
+	keycopy = strdup(key);
+	if (!keycopy)
+		return (0);
 	new_node->key = keycopy;
 	new_node->value = valuecopy;
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-
 	return (1);
-}
+
+
